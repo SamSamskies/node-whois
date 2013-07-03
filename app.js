db3b82d6d26220bc46a811bd7da03841177cb952
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-07-03 11:41:25 -0700

    Provide functions to covert keyword phrases to domain names and execute whois query on each domain name.

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
-#! /usr/bin/node
-
-// import
 var fs = require('fs');
-var exec = require('child_process').exec;
+var whois = require('whois');
+var file = "domainNames.txt"
 
-// 1. Add code to get the domainName.txt file as an argument passed to the app.
+function convertToDomainNames(keywords) {
+	names = keywords.replace(/ /g, '').split("\n")
+	return names.map(addDotCom)
+}
 
+function addDotCom(name) {
+	return name + ".com"
+}
 
 // read the file with the keyword phrases
-fs.readFile(__dirname + "/" + file, "Utf-8", function(err, data){
-	// 2. Add code here to covert each line into a domain name
+fs.readFile(__dirname + "/" + file, "Utf-8", function(err, fileContents){
+  if (err) { console.error(err.stack); return }
+
+  domainNames = convertToDomainNames(fileContents)
+
+  for (var dn in domainNames) {
+  	whois.query(domainNames[dn])
+  }
 });
-
-
-
-// function to query whois
-// 3. convert this to a custom module
-function query(name){
-	// make thh whois shell command
-	exec('whois ' + name, function(err, results){
-		var strResults = '';
-		if(results.indexOf("Expiration") > -1){
-			strResults = " is taken";
-		} else {
-			strResults = " is available";
-		}
-		console.log(name + strResults);
-	});
-}
